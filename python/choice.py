a93d05e5653cdd26992bdc4811deae254d58b2b0
Cole Herber <cherber@andrew.cmu.edu>
2024-02-22 09:09:45 -0500

    Mini-project 1 completed

--- python/choice.py
+++ python/choice.py
@@ -1,54 +1,114 @@
 import actr
-
+import numpy as np
 actr.load_act_r_model("ACT-R:tutorial;unit6;choice-model.lisp")
-
 choice_data = [0.664, 0.778, 0.804, 0.818]
-
 response = False
+virtual_mode = True
+
+
+def dominant_accuracy(response):
+    if response.count("h") > response.count("t"):
+        dominant = "h"
+    else:
+        dominant = "t"
+    return np.mean(np.array(response)==dominant)
+    #returns mean of response amounts
 
 def respond_to_key_press (model,key):
     global response
-
     response = key
 
-def person():
+def trial(window,probability,person=False):
     global response
 
-    if actr.visible_virtuals_available():
-        window = actr.open_exp_window("Choice Experiment",visible=True)
-    
-        actr.add_command("choice-response",respond_to_key_press,"Choice task key response")
-        actr.monitor_command("output-key","choice-response")
+    actr.add_command("choice-response",respond_to_key_press,"Choice task key response")
+    actr.monitor_command("output-key","choice-response")
 
-        actr.add_text_to_exp_window (window, 'choose', x=50, y=100)
+    actr.add_text_to_exp_window (window, 'choose', x=50, y=100)
 
-        response = ''
-            
+    response = ''
+
+    if person: 
         while response == '':
             actr.process_events()
+    else:
+        actr.run(1,not virtual_mode)
 
-        actr.clear_exp_window(window)
-    
-        if actr.random(1.0) < .9:
-            answer = 'heads'
-        else:
-            answer = 'tails'
+    actr.clear_exp_window(window)
 
-        actr.add_text_to_exp_window (window, answer, x=50, y=100)
+    if actr.random(1.0) < probability:
+        answer = 'heads'
+    else:
+        answer = 'tails'
 
-        start = actr.get_time(False)
-            
+    actr.add_text_to_exp_window (window, answer, x=50, y=100)
+
+    start = actr.get_time(False)
+
+    if person:
         while (actr.get_time(False) - start) < 1000:
-            actr.process_events()
+            actr.process_events()#IF NOT HERE RIDICULOUS PAUSE
+            #make sure person defaults to false
+    else:
+        actr.run(1,not virtual_mode)
+    actr.clear_exp_window(window)
+    actr.remove_command_monitor("output-key","choice-response")
+    actr.remove_command("choice-response")
 
-        actr.remove_command_monitor("output-key","choice-response")
-        actr.remove_command("choice-response")
- 
-        return response
+    return response
+
+def block(block_id,probability,person=False):
+    block_res = []
+    window = actr.open_exp_window("Choice Experiment",visible=False)
+    actr.install_device(window)
+    for i in range(12):
+        response = trial(window,probability,person)
+        block_res.append(response)
+    res = dominant_accuracy(block_res)
+
+    #print("Block-",block_id+1,"\tAccuracy=",hratio)
+    return res
 
 
-def model ():
-    None
+def model(num_block=4,probability=0.5,person=False):
+    result_list = []
+    for i in range(num_block):
+        result_list.append(block(i,probability,person))
+    return result_list
 
-def data (n):
-    None
+
+def data(n=100):#have to set variable because must have no required perameiters
+    window = actr.open_exp_window("Coin Flip Task",visible=True,width=300,height=300)
+    results = []#not numpy because numpy is bad at appending to lists
+    for i in range(n):
+        print(i)#anti hang code(2 hours i'm never getting back)┌( ಠ_ಠ )┘
+        results.append(model(probability=0.9))
+        results.append(model(probability=0.1))
+        #compares 200 runs
+
+    #convert to numpy for mean functions
+    results = np.array(results)
+    avg_result = list(np.mean(results,axis=0)) #need axis to get output list
+    correlation=actr.correlation(avg_result,choice_data)
+    mean_deviation=actr.mean_deviation(avg_result,choice_data)
+    
+    stats = f"""Original Current \n
+        {float(choice_data[0])} {float(avg_result[0])} \n
+        {float(choice_data[1])} {float(avg_result[1])} \n
+        {float(choice_data[2])} {float(avg_result[2])} \n
+        {float(choice_data[3])} {float(avg_result[3])}"""
+    corr = "CORRELATION: " + str(correlation)
+    md = "MEAN DEVIATION: " + str(mean_deviation)
+    print(corr)
+    print(md)
+    print(stats)
+    #forgot I had to display grrr
+    actr.add_text_to_exp_window(window, corr, x=20, y=50)
+    actr.add_text_to_exp_window(window, md, x=20, y=70)
+    actr.add_text_to_exp_window(window, stats, x=20, y=90)
+
+        
+    
+    
+#auto run for 100
+data(n=100) 
--- unit6/choice-model.lisp
+++ unit6/choice-model.lisp
@@ -1,7 +1,170 @@
-
 (clear-all)
 
-(define-model choice 
+(define-model choice-solution
 
- 
+(sgp :v nil :ul t  :egs 3 :esc t)
+(chunk-type goal state)
+(chunk-type response val)
+
+;;one semi-colon breaks colors
+(add-dm
+ (start isa chunk)
+ (attending-choose isa chunk) (respond isa chunk)
+ (attending-feedback isa chunk) (read-feedback isa chunk) (done isa chunk)
+ (goal isa goal state start)
+)
+
+(install-device '("motor" "keyboard"))
+
+(P start
+    =goal>
+       isa     goal
+       state   start
+==>
+    =goal>
+        isa goal
+        state attending-choose
+    +visual-location>
+)
+
+(P attend-letter
+   =goal>
+      ISA         goal
+      state       attending-choose
+   =visual-location>
+   ?visual>
+      state       free
+==>
+   +visual>
+      cmd         move-attention
+      screen-pos  =visual-location
+   =goal>
+      ISA         goal
+      state       respond
+)
+
+(p respond-t
+    =goal>
+      isa      goal
+      state    respond
+    =visual>
+      isa      visual-object
+      value    "choose"
+      ;;(!print! =val) 
+    ?manual>
+      state    free   
+    ?imaginal>
+      state    free
+   ==>
+    +manual>
+        cmd press-key
+        key "t"
+    +imaginal>
+        isa     response
+        val     "tails"
+    =goal>
+      isa      goal
+      state    attending-feedback
+    +visual-location>
+)
+
+
+
+(p respond-h
+    =goal>
+      isa      goal
+      state    respond
+    =visual>
+      isa      visual-object
+      value    "choose"
+      ;;(!print! =val)
+    ?manual>
+      state    free   
+    ?imaginal>
+      state    free
+   ==>
+    +manual>
+        cmd press-key
+        key "h"
+    +imaginal>
+        isa     response
+        val     "heads"
+    =goal>
+      isa      goal
+      state    attending-feedback
+    +visual-location>
+)
+
+
+(p attending-feedback
+    =visual-location>
+    =goal>
+      isa      goal
+      state    attending-feedback
+ ==>
+    =goal>
+      isa      goal
+      state    read-feedback
+    +visual-location>
+      :attended nil
+)
+
+
+
+(p read-feedback
+   =goal>
+       ISA goal
+       state read-feedback
+   =visual-location>
+   ?visual>
+      state       free
+==>
+   +visual>
+      cmd         move-attention
+      screen-pos  =visual-location
+   =goal>
+       ISA        goal
+       state      done
+
+)
+
+(p done-correct
+    =goal>
+      isa      goal
+      state    done
+    =visual>
+      isa      visual-object
+      value    =val
+    =imaginal>
+      val  =val
+   ==>
+    =goal>
+      state    start
+)
+
+(p done-wrong
+    =goal>
+      isa      goal
+      state    done
+    =visual>
+      isa      visual-object
+      value    =val
+    =imaginal>
+      -  val  =val
+   ==>
+    =goal>
+      state    start
+)
+
+(goal-focus goal)
+
+;;(spp respond-h :u 10)
+;;(spp respond-t :u 10)
+
+;;the :u value from spp indicates last utility value and noise
+
+(spp attend-letter :reward 0)
+(spp done-correct :reward 10)
+;;rewards
+
 )
